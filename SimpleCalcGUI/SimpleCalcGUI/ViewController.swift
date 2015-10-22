3e746bb87fdb9fbcf3990c80b28023bee4c60e23
Alex Schenck <AlexNSchenck@gmail.com>
2015-10-22 01:19:26 -0700

    Operations implemented. Not complete

--- SimpleCalcGUI/SimpleCalcGUI/ViewController.swift
+++ SimpleCalcGUI/SimpleCalcGUI/ViewController.swift
@@ -9,10 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +19,81 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var TxtFieldResult: UITextField!
+    var running : Double = -1
+    var op : String = ""
+    var operand : Double = -1
+    var count : Int = 0
+    
+    @IBAction func AppendNumberToField(sender: UIButton) {
+        if let text = sender.titleLabel?.text
+        {
+            TxtFieldResult.text! += text
+            
+            if (op == "") {
+                running = (text as NSString).doubleValue
+            }
+            else {
+                operand = (text as NSString).doubleValue
+            }
+        }
+    }
+    
+    @IBAction func AddOperation(sender: UIButton) {
+        if (op != "")
+        {
+            RunEquals(sender);
+        }
+        else
+        {
+            TxtFieldResult.text! = ""
+        }
+        
+        if let text = sender.titleLabel?.text
+        {
+            op = text
+        }
+    }
+    
+    @IBAction func RunEquals(sender: UIButton) {
+        switch op {
+            case "+":
+                running += operand
+            case "-":
+                running -= operand
+            case "x":
+                running *= operand
+            case "/":
+                running /= operand
+            case "avg":
+                running += operand
+                running /= Double(count)
+            case "count":
+                running = Double(count)
+            case "fact":
+                running = factorial(running)
+            default:
+                running = 0
+        }
+        
+        TxtFieldResult.text! = String(format:"%.1f", running)
+        operand = -1
+    }
+    
+    @IBAction func ClearField(sender: UIButton) {
+        TxtFieldResult.text! = ""
+        running = -1
+        op = ""
+        operand = -1
+    }
+    
+    func factorial(incoming: Double) -> Double {
+        if incoming < 2 {
+            return 1
+        }
+        else {
+            return incoming * factorial(incoming - 1)
+        }
+    }
 }
 
